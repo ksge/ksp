3c6f35045bd8a4896579b74f0a1270b6e699da74
ksge <44881679+ksge@users.noreply.github.com>
2024-04-14 21:34:18 +0000

    Delete compile-kspc-ksge-utils-runme.sh

--- compile-kspc-ksge-utils-runme.sh
+++
@@ -1,7 +0,0 @@
-../../FreeBASIC-1.07.3-linux-x86_64/bin/fbc kspc.bas
-sleep 1
-echo please remember to update kspc checksum inside ksge!
-../../FreeBASIC-1.07.3-linux-x86_64/bin/fbc KSGE-UTILS.bas
-sleep 1
-../../FreeBASIC-1.07.3-linux-x86_64/bin/fbc RUNME.bas
-sleep 5
